4b83c949b636bcb681c1f39a8fa8cf94bafbb0e9
kresnarmdn <raden.bagus11@ui.ac.id>
2023-06-11 20:24:49 +0900

    finished about page

--- Frontend/src/about/About.jsx
+++ Frontend/src/about/About.jsx
@@ -1,21 +1,22 @@
 import Footer from "../home/components/Footer";
 
 const About = () => {
+  document.documentElement.style.height = "100%";
+  document.body.style.height = "100%";
+
   return (
-    <div className="text-center py-8 h-fit align-middle text-textblue center overflow-y-clip">
-          <div className="flex-auto w-96 mt-64 mb-64 mx-auto my-auto">
-            CarGo is a car buying platform that makes it simple for you to buy
-            cars. We have a myriad of cars and other vehicles to choose from.
-            All of our cars are in good condition and are all available at
-            affordable prices.
+    <div className="text-center py-8 h-screen align-middle text-textblue center">
+      <div className="flex w-96 mt-64 mb-64 mx-auto my-auto">
+        CarGo is a car buying platform that makes it simple for you to buy
+        cars. We have a myriad of cars and other vehicles to choose from. All of
+        our cars are in good condition and are all available at affordable
+        prices.
 
-            This is a project made by Group W23 that used Tailwind CSS.
-          </div>
-          <Footer className="">
-
-          </Footer>
+        This is a project made by Group W23 that used Tailwind CSS.
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default About;
